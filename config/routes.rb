8a97878fc0cbe8a863a49fe27bf3e66e538476d5
piy9 <piyushkhemka123@gmail.com>
2013-10-27 21:02:08 +0530

    Added layout links with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 First::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/contact"
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'  
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
   
-  get "pages/about"
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+ 
+
+
+ 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,10 +59,6 @@ First::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
